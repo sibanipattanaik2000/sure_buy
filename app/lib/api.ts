86bbf9d8d3676b064311788358bb5b6c61950877
sibanipattanaik2000 <sibanipattanaik200@gmail.com>
2026-08-24 16:08:08 +0530

    added updated

---
+++ app/lib/api.ts
@@ -0,0 +1,29 @@
+const API_URL =
+  process.env.NEXT_PUBLIC_API_URL ||
+  "http://localhost:5000/api";
+
+export async function apiFetch<T>(
+  endpoint: string,
+  options?: RequestInit,
+): Promise<T> {
+  const response = await fetch(
+    `${API_URL}${endpoint}`,
+    {
+      ...options,
+      headers: {
+        "Content-Type": "application/json",
+        ...(options?.headers || {}),
+      },
+    },
+  );
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(
+      data?.message || "Something went wrong",
+    );
+  }
+
+  return data;
+}
